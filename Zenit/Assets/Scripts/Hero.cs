fa86c32cc027da1e99f74c8189aed03427f124ea
Kostrula <d.galkin2011@yandex.ru>
2020-09-30 09:04:25 +0300

    Added sritessheet and some Tilemaps

---
+++ Zenit/Assets/Scripts/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private Vector2 moveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime); 
+        
+    }
+}
