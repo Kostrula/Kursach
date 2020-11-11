4335a7a5d53ac389a5d0a4d42629893611ae9b25
Kostrula <d.galkin2011@yandex.ru>
2020-11-11 15:47:51 +0300

    ADded nnew Scene

--- Zenit/Assets/Scripts/CameraController.cs
+++ Zenit/Assets/Scripts/CameraController.cs
@@ -10,49 +10,61 @@ public class CameraController : MonoBehaviour
 
     private Vector3 offset;
 
+    public bool CanMove;
+
 
     void Start()
     {
-        offset = transform.position - Player.transform.position;
+        if (CanMove)
+        {
+            offset = transform.position - Player.transform.position;
+        }
+        else
+        {
+            transform.position.Set(Player.transform.position.x, Player.transform.position.y, -10);
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (Player.transform.position.x > 45.0f)
+        if (CanMove)
         {
-            float verticalInput = Player.transform.position.y ;
-            transform.position = new Vector3(45.0f, verticalInput, -5.0f) + offset;
-            if (Player.transform.position.y <= -15.0f)
+            if (Player.transform.position.x > 45.0f)
             {
-                transform.position = new Vector3(45.0f, -15.0f, -5.0f) + offset;
-            }
-        }
-        else
-        {
-            if (Player.transform.position.y <= -15.0f)
-            {
-                float horizontalInput = Player.transform.position.x;
-                transform.position = new Vector3(horizontalInput, -15.0f, -5.0f) + offset;
-                if (Player.transform.position.x < -28.5f)
+                float verticalInput = Player.transform.position.y;
+                transform.position = new Vector3(45.0f, verticalInput, -5.0f) + offset;
+                if (Player.transform.position.y <= -15.0f)
                 {
-                    float verticalInput = Player.transform.position.y;
-                    transform.position = new Vector3(-28.5f, -15.0f, -5.0f) + offset;
+                    transform.position = new Vector3(45.0f, -15.0f, -5.0f) + offset;
                 }
             }
             else
             {
-                if (Player.transform.position.x < -28.5f)
+                if (Player.transform.position.y <= -15.0f)
                 {
-                    float verticalInput = Player.transform.position.y;
-                    transform.position = new Vector3(-28.5f, verticalInput, -5.0f) + offset;
+                    float horizontalInput = Player.transform.position.x;
+                    transform.position = new Vector3(horizontalInput, -15.0f, -5.0f) + offset;
+                    if (Player.transform.position.x < -28.5f)
+                    {
+                        float verticalInput = Player.transform.position.y;
+                        transform.position = new Vector3(-28.5f, -15.0f, -5.0f) + offset;
+                    }
                 }
                 else
                 {
-                    transform.position = Player.transform.position + offset;
+                    if (Player.transform.position.x < -28.5f)
+                    {
+                        float verticalInput = Player.transform.position.y;
+                        transform.position = new Vector3(-28.5f, verticalInput, -5.0f) + offset;
+                    }
+                    else
+                    {
+                        transform.position = Player.transform.position + offset;
+                    }
                 }
-            }
 
+            }
         }
 
     }
